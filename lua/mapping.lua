d6d0431c0f4fdd7085f0a20e1d64892f26af7140
Levi Rizki Saputra <42236775+levirs565@users.noreply.github.com>
2021-03-25 19:09:04 +0700

    add lsp mapping

--- lua/mapping.lua
+++ lua/mapping.lua
@@ -1,4 +1,8 @@
 local set = vim.api.nvim_set_keymap
+local opts = {
+  noremap = true,
+  -- silent = true
+}
 
 set("n", "<A-l>", "<CMD>BufferNext<CR>", {noremap = true, silent = true})
 set("n", "<A-h>", "<CMD>BufferPrevious<CR>", {noremap = true, silent = true})
@@ -23,3 +27,28 @@ end
 
 map_telescope("<A-n>", "find_files")
 map_telescope("<A-f>", "live_grep")
+
+local function map_lsp(key, fn_name)
+  set("n", key, "<CMD>lua vim.lsp." .. fn_name .. "()<CR>", opts)
+end
+
+local function map_lsp_buf(key, fn_name)
+  map_lsp(key, "buf." .. fn_name)
+end
+
+local function map_lsp_diag(key, fn_name)
+  map_lsp(key, "diagnostic." .. fn_name)
+end
+
+map_lsp_buf("gd", "type_definition")
+map_lsp_buf("gD", "definition")
+map_lsp_buf("gi", "implementation")
+map_lsp_buf("gr", "references")
+map_lsp_buf("K", "hover")
+map_lsp_buf("<C-k>", "signature_help")
+map_lsp_buf("<leader>rn", "rename")
+map_lsp_buf("<leader>f", "formatting")
+map_lsp_diag("<leader>e", "show_line_diagnostics")
+map_lsp_diag("<leader>le", "set_loclist")
+map_lsp_diag("[d", "goto_prev")
+map_lsp_diag("]d", "goto_next")
